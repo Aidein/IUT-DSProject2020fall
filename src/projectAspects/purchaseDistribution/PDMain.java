8fc0e67acd437c36919e4c148a3339ed57185b97
Aidein <sultanovazamat985@gmail.com>
2020-12-25 14:35:17 +0500

    gitignore added

--- src/projectAspects/purchaseDistribution/PDMain.java
+++ src/projectAspects/purchaseDistribution/PDMain.java
@@ -1,36 +1,42 @@
 package projectAspects.purchaseDistribution;
 
 public class PDMain {
-    public static void main(String[] args){
-        IngredientInterface meat = new Ingredient("Meat");
-        IngredientInterface tomato = new Ingredient("Tomato");
-        IngredientInterface potato = new Ingredient("Potato");
-        IngredientInterface apple = new Ingredient("Apple");
-        IngredientInterface chicken = new Ingredient("Chicken");
+//    public static void main(String[] args){
+//        IngredientInterface meat = new Ingredient("Meat");
+//        IngredientInterface tomato = new Ingredient("Tomato");
+//        IngredientInterface potato = new Ingredient("Potato");
+//        IngredientInterface apple = new Ingredient("Apple");
+//        IngredientInterface chicken = new Ingredient("Chicken");
+//
+//        MealInterface meal1 = new Meal("Meal1");
+//
+//        meal1.attach(meat);
+//        meal1.attach(tomato);
+//        meal1.attach(potato);
+//
+//        System.out.println(meal1.toString());
+//
+//        MealInterface meal2 = new Meal("Meal2");
+//
+//        meal2.attach(potato);
+//        meal2.attach(tomato);
+//        meal2.attach(apple);
+//        meal2.attach(chicken);
+//
+//        System.out.println(meal2.toString());
+//
+//        System.out.println("Please set the price:");
+//        meat.setPrice(57000);
+//        potato.setPrice(5000);
+//        tomato.setPrice(7000);
+//        apple.setPrice(13000);
+//        chicken.setPrice(27000);
+//        System.out.printf("Prices:\n1) %s\n2) %s\n3) %s\n", meat.toString(), chicken.toString(), apple.toString());
+//    }
+    // ingredient counter
+    private int numberOfIngredients = 0;
 
-        MealInterface meal1 = new Meal("Meal1");
-
-        meal1.attach(meat);
-        meal1.attach(tomato);
-        meal1.attach(potato);
-
-        System.out.println(meal1.toString());
-
-        MealInterface meal2 = new Meal("Meal2");
-
-        meal2.attach(potato);
-        meal2.attach(tomato);
-        meal2.attach(apple);
-        meal2.attach(chicken);
-
-        System.out.println(meal2.toString());
-
-        System.out.println("Please set the price:");
-        meat.setPrice(57000);
-        potato.setPrice(5000);
-        tomato.setPrice(7000);
-        apple.setPrice(13000);
-        chicken.setPrice(27000);
-        System.out.printf("Prices:\n1) %s\n2) %s\n3) %s\n", meat.toString(), chicken.toString(), apple.toString());
+    public void setIngredient(String name){
+        
     }
 }
